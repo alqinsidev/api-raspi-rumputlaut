18899c156985ff72bd477079311dec7083fbc0bd
alqinsidev <padlanalqinsi@live.com>
2019-10-16 20:11:06 +0700

    node modules

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
-const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
+const port = new SerialPort('COM17', { baudRate: 9600 });
 const parser = port.pipe(new Readline({ delimiter: '\n' }));
-var data_sensor;
+const fs = require('fs');
+var moment = require('moment');
+const axios = require('axios');
+
+var d = new Date();
+var n = d.getTime();
+var curdat;
 
 parser.on('data', function (data) {
  if(data.substring(0,1) ==  '{')
  {
-   data_sensor = JSON.parse(data);
-   console.log(data_sensor);
+   curdat = JSON.parse(data);
+   fs.writeFile(__dirname+'/datalogger/' + curdat.id + '-' + moment(d).format('YYYY-MM-DD hh-mm') +'.json', data, function (err) {
+     if (err) throw err;
+     console.log('Saved!');
+     axios.post('http://localhost:3000/data/' + curdat.id, curdat)
+   });
+
+
  }
 })
