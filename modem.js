3594e1ff06d5b784aa9bf0e4507b6bc9b5e87c63
alqinsidev <padlanalqinsi@live.com>
2019-10-16 07:13:28 +0700

    Update modem.js

--- modem.js
+++ modem.js
@@ -4,13 +4,9 @@ modem.on('open', function() {
   // do something with the modem
   modem.execute('AT').then(function(lines) {
   console.log('AT Response', lines);
-}, function(error) {
-  console.error('AT Command Error', error);
-});
+}
 });
 
-modem.error('error', function() {
 
-});
 
 modem.open(); // open the port
