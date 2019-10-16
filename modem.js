67788237a215ab1c2486beab4d0d8e233a3fc8aa
alqinsidev <padlanalqinsi@live.com>
2019-10-16 07:19:24 +0700

    Update modem.js

--- modem.js
+++ modem.js
@@ -1,19 +1,14 @@
-'use strict';
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('/dev/ttyS0', { baudRate: 9600 });
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
+var data_sensor;
 
-// require lib
-const Port = require('serial-at');
-
-(async function main() {
-    // create serial connection
-    const port = new Port('/dev/ttyS0', {baudRate:9600});
-
-
-    // open serial connection
-    await port.open();
-
-    // execute AT command and diaplay result
-    console.log(await port.at('AT'));
-
-    // close serial connection
-    await port.close();
-})();
+port.write('AT', function(err) {
+  if (err) {
+    return console.log('Error on write: ', err.message)
+  }
+  parser.on('data', function (data) {
+    console.log(data);
+  })
+})
