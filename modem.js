6dafa1dd09326fa26bf914830c55415c969cbe0e
alqinsidev <padlanalqinsi@live.com>
2019-10-16 07:52:46 +0700

    Update modem.js

--- modem.js
+++ modem.js
@@ -1,44 +1,29 @@
-const serialportgsm = require('serialport-gsm');
-
-var gsmModem = serialportgsm.Modem()
+let serialportgsm = require('serialport-gsm')
+let modem = serialportgsm.Modem()
 let options = {
-  baudRate: 9600,
-  dataBits: 8,
-  parity: 'none',
-  stopBits: 1,
-  xon: false,
-  rtscts: false,
-  xoff: false,
-  xany: false,
-  autoDeleteOnReceive: true,
-  enableConcatenation: true,
-  incomingCallIndication: true,
-  incomingSMSIndication: true,
-  pin: '',
-  customInitCommand: 'AT^CURC=0',
-  logger: console
+    baudRate: 9600,
+    dataBits: 8,
+    stopBits: 1,
+    parity: 'none',
+    rtscts: false,
+    xon: false,
+    xoff: false,
+    xany: false,
+    autoDeleteOnReceive: true,
+    enableConcatenation: true,
+    incomingCallIndication: true,
+    incomingSMSIndication: true,
+    pin: '',
+    customInitCommand: '',
+    logger: console
 }
 
+modem.open('/dev/ttyS0', options,()=>{
+  console.log("opening modem port");
+});
 
-
-// Port is opened
 modem.on('open', data => {
-  gsmModem.initializeModem((msg, err) => {
- if (err) {
-   console.log(`Error Initializing Modem - ${err}`);
- } else {
-   console.log(`InitModemResponse: ${JSON.stringify(msg)}`);
-   // execute a custom command - one line response normally is handled automatically
-          gsmModem.executeCommand('AT+CPIN?', (result, err) => {
-            if (err) {
-              console.log(`Error - ${err}`);
-            } else {
-              console.log(`Result ${JSON.stringify(result)}`);
-            }
-          });
-}
+    modem.initializeModem(()=>{
+      console.log("modem terbuka");
+    })
 })
-
-
-
-gsmModem.open('/dev/ttyS0', options);
